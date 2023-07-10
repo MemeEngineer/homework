c050d04ad512ea98b592b975b9d3fbf23a65f6a7
JWu <johnnyw131@gmail.com>
2023-07-10 02:33:34 -0400

    Excited kittens answered

--- answers.js
+++ answers.js
@@ -11,3 +11,13 @@ for(let i = 0; i <= 200; i++){
     }
 }
 
+//======= Excited Kitten =======
+
+let arr= [ "...human...why you taking pictures of me?...", "...the catnip made me do it...", "...why does the red dot always get away..."]
+for(let i=0; i <= 20; i++){
+    if(i % 2 === 0){
+        let meow = 0
+        console.log(arr[Math.floor(Math.random()*arr.length)])
+
+        }   
+     }
