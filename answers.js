99dc81c21b166029f62e5610f928bdc77c323ec0
JWu <johnnyw131@gmail.com>
2023-07-10 02:45:34 -0400

    Find the median answered

--- answers.js
+++ answers.js
@@ -21,3 +21,8 @@ for(let i=0; i <= 20; i++){
 
         }   
      }
+//============== Find the median ============
+
+const nums = [14,11,16,15,13,16,15,17,19,11,12,14,19,11,15,17,11,18,12,17,12,71,18,15,12];
+
+console.log(nums.sort()[Math.floor(nums.length/2)])
